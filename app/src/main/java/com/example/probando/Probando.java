00ce1a653acda57e621de3a64ff372f8f779e260
EricJess2001 <ericjessid2018@gmail.com>
2023-05-25 12:39:16 -0500

    mejorando

---
+++ app/src/main/java/com/example/probando/Probando.java
@@ -0,0 +1,36 @@
+package com.example.probando;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.probando.databinding.FragmentProbandoBinding;
+
+
+public class Probando extends Fragment {
+
+    private FragmentProbandoBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentProbandoBinding.inflate(inflater,container,false);
+        View root = binding.getRoot();
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
--- app/src/main/java/com/example/probando/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/probando/ui/home/HomeFragment.java
@@ -1,7 +1,6 @@
 package com.example.probando.ui.home;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,32 +9,37 @@ import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.probando.Principalblog;
+import com.example.probando.Probando;
 import com.example.probando.R;
 import com.example.probando.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
-    private LinearLayout tem1;
     private FragmentHomeBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
-
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        tem1 = root.findViewById(R.id.tem1);
+        LinearLayout tem1 = root.findViewById(R.id.tem1);
         tem1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent irsiguiente = new Intent(getActivity(), Principalblog.class);
-                startActivity(irsiguiente);
+            public void onClick(View v) {
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                Fragment fragment = new Probando(); // Reemplaza "YourFragment" con la clase de tu fragmento destino
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, fragment)
+                        .addToBackStack(null)
+                        .commit();
             }
         });
+
+
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         return root;
     }
